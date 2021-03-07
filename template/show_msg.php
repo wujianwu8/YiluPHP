0229589a35363f2c188a588b647697cd2a735790
WuJianwu <7594870087@qq.com>
2021-03-08 00:28:17 +0800

    优化错误显示页

--- template/show_msg.php
+++ template/show_msg.php
@@ -1,9 +1,24 @@
-<!--{use_layout layout/main}-->
-<?php
-    $head_info = [
-        'title' => $msg.'('.$err_code.')',
-    ];
-?>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta http-equiv=Content-Type content="text/html;charset=utf-8">
+    <title><?php echo $msg.'('.$err_code.')'; ?></title>
+    <?php if($err_code===-1): ?>
+    <script>
+        alert("<?php echo htmlspecialchars($msg); ?>");
+        document.location.href = "/?redirect_uri="+encodeURIComponent(document.location.href);
+    </script>
+    <?php endif; ?>
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?802be9112dbcdf29bc10f0eabed49dca";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+</head>
+<body>
 
 <div class="error">
     <p><?php echo $msg; ?></p>
@@ -16,10 +31,13 @@
 </div>
 
 <?php if (!empty($backtrace)): ?>
-<div style="background: peachpuff; color: #d00000;">
-    <h3 style="background: brown; color: gold; padding: 0.3rem 1rem;"><?php echo YiluPHP::I()->lang('debug_mode_title'); ?></h3>
-    <pre style=" padding: 0.5rem 1.5rem 0.5rem 1.5rem;">
+    <div style="background: peachpuff; color: #d00000;">
+        <h3 style="background: brown; color: gold; padding: 0.3rem 1rem;"><?php echo YiluPHP::I()->lang('debug_mode_title'); ?></h3>
+        <pre style=" padding: 0.5rem 1.5rem 0.5rem 1.5rem;">
 <?php print_r($backtrace); ?>
     </pre>
-</div>
+    </div>
 <?php endif; ?>
+
+</body>
+</html>
